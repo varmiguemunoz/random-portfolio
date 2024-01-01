4073acf92268dcf410a4bcff0b581f713ebb4211
varmiguemunoz <varmiguemunoz@gmail.com>
2023-12-31 22:48:07 -0500

    add 🧠: new interface

--- src/template/template.js
+++ src/template/template.js
@@ -6,21 +6,19 @@ const Template = async () => {
     <div class="About">
       <div class="card">
         <div class="card_details">
-          <div class="card_photo center circle">
+          <div class="card_photo circle">
             <img src="${data.picture.large}" alt="${data.name.first}">
             <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" style="enable-background:new -580 439 577.9 194;"
               xml:space="preserve">
               <circle cx="50" cy="50" r="40" />
             </svg>
           </div>
-          <p class="card_title">Hi, My name is</p>
+          <p class="card_title">Hey, my name is: </p>
           <p class="card_value">${data.name.first} ${data.name.last}</p>
         </div>
         <div class="card_userdata">
-          <ul>
-            <li>${data.email}</li>
-            <li>${data.location.country}</li>
-          </ul>
+            <p>Email: ${data.email}</p>
+            <p>I am from: ${data.location.country}</p>
         </div>
         <div class="card_social">
           <a href="https://twitter.com/gndx">
